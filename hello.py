3944eb6162c05c2186f51a8fb1fc814e6a3b02a6
Michael Pradel <michael@binaervarianz.de>
2023-08-24 09:31:10 +0200

    Adding suppression of the warning

--- hello.py
+++ hello.py
@@ -4,5 +4,5 @@ def toy():
     """toy example"""
     var_a = 23
     var_b = 42
-    var_c = var_a + var_b + var_x
+    var_c = var_a + var_b + var_x  # pylint: disable=undefined-variable
     return var_c
